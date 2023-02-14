af22a4c625667deb965641f60aecf4a4b89f7868
officialrajdeepsingh <radewala.2@gmail.com>
2023-02-14 19:52:48 +0530

    adding the ThemeProvide warpper for demo

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '@/styles/globals.css'
+import {ThemeProvider} from '@primer/react'
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  return <ThemeProvider colorMode="auto"
+  dayScheme="light"
+  nightScheme="dark" preventSSRMismatch>
+      <Component {...pageProps} />
+  </ThemeProvider> 
 }
